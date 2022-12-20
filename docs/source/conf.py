0e73a22c485498c9d6165555810c3206379379d2
Kyle Pidgeon <92022089+kpidge@users.noreply.github.com>
2022-12-20 12:18:33 +0000

    Add config for version switcher in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,6 +86,12 @@ pygments_style = None
 #
 html_theme = 'pydata_sphinx_theme'
 
+html_theme_options = {
+    "switcher": {
+        "json_url": "https://tomographicimaging.github.io/CIL/nightly/_static/switcher.json",
+    }
+}
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
